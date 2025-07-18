2484520cdb2793d3b03784b6c08b3e2c6a74a2b9
arielbeje <arielbeje@protonmail.com>
2025-07-18 13:08:06 +0300

    First iteration - hardcoded memory DB, doing nothing with flaky tests
    We require pytest-rerunfailures to detect flaky-ness.
    
    Using hardcoded GitLab CI variables for defaults.

---
+++ melt/flaky_test_db.py
@@ -0,0 +1,70 @@
+import datetime
+import os
+from typing import Iterator
+
+from peewee import (
+    AutoField,
+    DateTimeField,
+    ForeignKeyField,
+    IntegerField,
+    Model,
+    SqliteDatabase,
+    TextField,
+)
+
+db = SqliteDatabase(":memory:")
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class FlakyTest(BaseModel):
+    id_ = AutoField(column_name="id")
+    node_id = TextField(unique=True)
+    first_seen = DateTimeField(default=datetime.datetime.now, index=True)
+    last_updated = DateTimeField(default=datetime.datetime.now, index=True)
+
+
+class FlakyTestRun(BaseModel):
+    id_ = AutoField(column_name="id")
+    test = ForeignKeyField(FlakyTest)
+    job_name = TextField(default=os.environ["CI_JOB_NAME"])
+    job_id = IntegerField(default=int(os.environ["CI_JOB_ID"]))
+    mr_id = IntegerField(default=int(os.environ["CI_MERGE_REQUEST_IID"]))
+    pipeline_id = IntegerField(default=int(os.environ["CI_PIPELINE_IID"]))
+    commit_hash = TextField(default=os.environ["CI_COMMIT_SHA"])
+    timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+
+    class Meta:
+        indexes = (
+            # Create a unique index - there should only be one instance of a test
+            # for each job.
+            (("test", "job_id"), True),
+        )
+
+
+def get_flaky_tests(max_age: datetime.timedelta) -> Iterator[FlakyTest]:
+    after = datetime.datetime.now() - max_age
+    return FlakyTest.select().where(FlakyTest.last_updated >= after)
+
+
+def log_flaky_test_run(nodeid: str) -> None:
+    (
+        # last_updated defaults to now
+        FlakyTest.insert(node_id=nodeid)
+        .on_conflict(
+            # The test might already exist in the DB
+            conflict_target=(FlakyTest.node_id,),
+            # Only update last_updated, keeping the DB's first_seen
+            preserve=(FlakyTest.last_updated,),
+        )
+        .execute()
+    )
+
+    test = FlakyTest.get(FlakyTest.node_id == nodeid)
+    FlakyTestRun.create(test=test, commit_hash="fefef")
+
+
+db.create_tables((FlakyTest, FlakyTestRun))
---
+++ melt/pytest_hooks.py
@@ -0,0 +1,36 @@
+import datetime
+
+import pytest
+
+from melt.flaky_test_db import get_flaky_tests, log_flaky_test_run
+
+FLAKY_TEST_UPDATED_THRESHOLD = datetime.timedelta(days=1)
+
+
+# Global variable since we need to conserve state between hook calls
+rerun_tests = set()
+
+
+def pytest_runtest_logreport(report):
+    # We don't care about setup/teardown
+    if report.when != "call":
+        return
+
+    if report.rerun:
+        rerun_tests.add(report.nodeid)
+
+    # If the test passed, but had previous failures, it's flaky
+    if report.passed and report.nodeid in rerun_tests:
+        log_flaky_test_run(report.nodeid)
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_collection_modifyitems(items):
+    flaky_node_ids = {
+        test.node_id for test in get_flaky_tests(FLAKY_TEST_UPDATED_THRESHOLD)
+    }
+
+    for item in items:
+        if item.nodeid in flaky_node_ids:
+            # item.add_marker(pytest.mark.skip("Flaky"))
+            pass
