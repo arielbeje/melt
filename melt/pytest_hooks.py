26a85c1f29e6ad3bc45ddebc2f20b688a5c1e982
arielbeje <arielbeje@protonmail.com>
2025-07-18 21:17:30 +0300

    pytest_hooks: Rename FLAKY_TEST_UPDATE_THRESHOLD to FLAKY_TEST_MAX_AGE

--- melt/pytest_hooks.py
+++ melt/pytest_hooks.py
@@ -9,8 +9,8 @@ from melt.flaky_test_db import (
     log_flaky_test_run,
 )
 
-FLAKY_TEST_UPDATED_THRESHOLD = datetime.timedelta(days=1)
-FLAKY_TEST_UPDATED_THRESHOLD_HOURS = FLAKY_TEST_UPDATED_THRESHOLD.total_seconds() / 3600
+FLAKY_TEST_MAX_AGE = datetime.timedelta(days=1)
+FLAKY_TEST_MAX_AGE_HOURS = FLAKY_TEST_MAX_AGE.total_seconds() / 3600
 IMPACTED_MERGE_REQUEST_SKIP_THRESHOLD = 3
 
 
@@ -44,7 +44,7 @@ class MeltPlugin:
 
     @pytest.hookimpl(trylast=True)
     def pytest_collection_modifyitems(self, items):
-        flaky_tests = list(get_flaky_tests(FLAKY_TEST_UPDATED_THRESHOLD))
+        flaky_tests = list(get_flaky_tests(FLAKY_TEST_MAX_AGE))
         node_id_to_test = {test.node_id: test for test in flaky_tests}
 
         for item in items:
@@ -54,13 +54,13 @@ class MeltPlugin:
             if item.nodeid in node_id_to_test:
                 test = node_id_to_test[item.nodeid]
                 impacted_merge_requests = count_impacted_merge_requests(
-                    test, FLAKY_TEST_UPDATED_THRESHOLD
+                    test, FLAKY_TEST_MAX_AGE
                 )
                 if impacted_merge_requests >= IMPACTED_MERGE_REQUEST_SKIP_THRESHOLD:
                     item.add_marker(
                         pytest.mark.skip(
                             f"Flaky - impacted {impacted_merge_requests} merge requests "
-                            f"in the last {int(FLAKY_TEST_UPDATED_THRESHOLD_HOURS)} hours "
+                            f"in the last {int(FLAKY_TEST_MAX_AGE_HOURS)} hours "
                             f"(last update: {test.last_updated.isoformat(sep=' ', timespec='minutes')})"
                         )
                     )
